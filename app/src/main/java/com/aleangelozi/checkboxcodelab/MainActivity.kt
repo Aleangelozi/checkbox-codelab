0e7b745a020f356664ca54b123a6126d2b5c4dfe
aleangelozi <aleangelozi@gmail.com>
2022-02-23 13:15:36 -0300

    Initial commit

---
+++ app/src/main/java/com/aleangelozi/checkboxcodelab/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.aleangelozi.checkboxcodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onCheckboxClicked(view: View) {
+        if (view is CheckBox) {
+            val checked: Boolean = view.isChecked
+
+            when (view.id) {
+                R.id.checkbox_meat -> {
+                    if (checked) {
+                        // Put some meat on the sandwich
+                    } else {
+                        // Remove the meat
+                    }
+                }
+                R.id.checkbox_cheese -> {
+                    if (checked) {
+                        // Cheese me
+                    } else {
+                        // I'm lactose intolerant
+                    }
+                }
+                // TODO: Veggie sandwich
+            }
+        }
+    }
+}
